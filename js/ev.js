cafdd33ea1e4ca37fa11f6f961829a1e43f31820
Fred Chasen <fchasen@gmail.com>
2013-04-24 11:42:40 -0700

    init

---
+++ js/ev.js
@@ -0,0 +1,121 @@
+var events = [
+	{
+		"y" : 1880,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 0
+		//"c" : "+"
+	},
+	{
+		"y" : 1890,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : -150
+	},
+	{
+		"y" : 1900,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 25
+
+	},
+	{
+		"y" : 1910,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 30,
+		"c" : "+"
+	},
+	{
+		"y" : 1914,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 80,
+		"c" : "-"
+	},
+	{
+		"y" : 1954,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 180
+	},
+	{
+		"y" : 1914,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : -65
+
+	},
+	{
+		"y" : 1934,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : -25,
+		"c" : "-"
+
+	},
+	{
+		"y" : 1954,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 0
+
+	},
+	{
+		"y" : 1994,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : -50
+
+	},
+	{
+		"y" : 2013,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["cc"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : 50,
+	},
+	{
+		"y" : 2013,
+		"m" : "March",
+		"d" : "8",
+		"v" : ["bus"],
+		"desc" : "bllllah",
+		"img" : "images/1.jpg",
+		"pos" : -150
+
+	},
+
+];
