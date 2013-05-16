be18ef4aac2665cf149705f6e7cb2b48d6a82ec1
Fred Chasen <fchasen@gmail.com>
2013-05-16 13:25:31 -0700

    slowed down travel

--- js/app.js
+++ js/app.js
@@ -772,7 +772,7 @@ MM.travel = function(line, startat) {
 		config = { stroke: 'white', fill: 'none', 'fill-opacity': 0, 'stroke-width': 3},
 		plen = route.events.length - 1,
 		cur = startat || 0,
-		delay = 5500,
+		delay = 6200,
 		st;
 		
 	if(!MM.travelLines[line]){
